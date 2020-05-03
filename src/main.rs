250a64c53370eb5f290de8e37caf2ef1ad98f7cb
Phong Nguyen <phongnt.ptnk@gmail.com>
2020-05-04 03:11:49 +0800

    Hello world async

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let resp = reqwest::get("https://httpbin.org/ip")
+    .await?
+    .json::<HashMap<String, String>>()
+    .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
